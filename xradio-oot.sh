dabb4d7aaa2afbc6259ab0e1dfdc0575efb5e0fe
karabek <karabek@users.noreply.github.com>
2017-07-23 20:34:37 +0200

    some decorations ...

--- xradio-oot.sh
+++ xradio-oot.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 if [[ $EUID != 0 ]]; then
-	echo -e "This script requires [\e[0;35m ROOT \x1B[0m] privileges."
+	echo -e "This script requires \e[0;35mROOT\x1B[0m privileges."
 	sudo "$0" "$@"
 	exit $?
 fi
 echo ""
-echo "***********************************************"
-echo "*** xradio out-of-tree driver installer 0.1 ***"
-echo "***********************************************"
+echo "******************************************************"
+echo "*** out-of-tree xradio driver installer - vers 0.1 ***"
+echo "*** https://github.com/karabek/xradio  (c) karabek ***"
+echo "******************************************************"
 echo ""
 kvers="$(uname -r)"
-if [ -d /lib/modules/$kvers/kernel/drivers/net/wireless/xradio ]; then
-	echo "xradio installed already?"
-	exit
-fi
+#if [ -d /lib/modules/$kvers/kernel/drivers/net/wireless/xradio ]; then
+#	echo "xradio installed already?"
+#	exit
+#fi
 echo "=============== compiling driver for kernel version $kvers"
 make  -C /lib/modules/$kvers/build M=$PWD modules
 mkdir /lib/modules/$kvers/kernel/drivers/net/wireless/xradio
 cp xradio_wlan.ko /lib/modules/$kvers/kernel/drivers/net/wireless/xradio/
-echo -e "xradio_wlan" >> /etc/modules
+xmod=`grep xradio /etc/modules`
+if [ -z "$xmod" ]; then
+        echo -e "xradio_wlan" >> /etc/modules
+fi
+echo "=============== calling depmod"
 depmod
+echo "=============== adding overlay"
 armbian-add-overlay xradio-mrk1.dts
 exit
 
